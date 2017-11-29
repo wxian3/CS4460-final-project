5c9636c2335bcec66e070515507e7e51c919de6f
Joon Gyu Choi <jchoi307@gmail.com>
2017-11-29 18:41:02 -0500

    Putted Location csv data
    Couldn't fixed the weird points on the map yet

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ var nodes;
 var width = document.getElementById('main').offsetWidth;
 var height = width / 2;
 
+var locData;
+var locNodes;
+
 var graticule = d3.geoGraticule();
 
 var projection = d3.geoMercator()
@@ -33,6 +36,16 @@ d3.json("data/world-topo-min.json", function(error, world) {
     setupMap(countries);
 });
 
+//additional dataset for location lat,long
+d3.csv("data/World_Cities_Location.csv", function(err, dataset2) {
+    locData = dataset2;
+    locNodes = d3.nest().key(function(d){
+        return d.City;
+    })
+    .entries(locData);
+    // console.log(locNodes);
+});
+
 function setupMap(countries) {
 
     svg.append("path")
@@ -50,6 +63,7 @@ function setupMap(countries) {
         .attr("title", function(d,i) { return d.properties.name; })
         .style("fill", function(d, i) { return d.properties.color; });
 
+
     // Project location of data points on the map
     d3.csv("data/aircraft_incidents_edit.csv", function(err, dataset) {
 
@@ -59,8 +73,8 @@ function setupMap(countries) {
 
         allData = dataset;
         nodes = d3.nest()
-            .key(function(d){ 
-                return d.Event_Date
+            .key(function(d){
+                return d.Event_Date;
             })
             .entries(allData);
 
@@ -80,7 +94,6 @@ function updateChart(year) {
     //     projectPoints(d.values[0].Longitude, d.values[0].Latitude, d.values[0].Location );
     // });
 
-
     // TODO: How to enter/update/merge/remove this below function
     // timeData.forEach(function(d){
     //     projectPoints(d.Longitude, d.Latitude, d.Location );
@@ -93,23 +106,65 @@ function updateChart(year) {
         });
     //var x = projection([lat,lon])[0];
     //var y = projection([lat,lon])[1];
-    var radius = 3
-    
+    var radius = 3;
+    var x = 0;
+    var y = 0;
+
     var geo_enter = geo_point.enter()
         .append('g')
-        .attr('class', 'geo_point')
-    
-    geo_point.merge(geo_enter)
+        .attr('class', 'geo_point');
+
+    geo_point.merge(geo_enter);
 
     geo_enter.append("svg:circle")
         .attr("cx", function(d) {
             if (d.values[0].Latitude.length != 0) {
-                return projection([d.values[0].Latitude,d.values[0].Longitude])[0]
+            	x = projection([d.values[0].Latitude,d.values[0].Longitude])[0];
+                return x;
+            } else {
+                var locName = d.values[0].Location.split(',');
+                console.log(locName[0]);
+                // console.log(locNodes);
+                var locFilteredValue = locNodes.filter(function(d) {
+                    if (d.key.toLowerCase() == locName[0].toLowerCase()) {
+                        return d.key;
+                    };
+                });
+                // console.log(locFilteredValue);
+                if (typeof(locFilteredValue[0]) !== 'undefined') {
+                    // console.log(locFilteredValue[0].values[0].Latitude);
+                    var lat2 = locFilteredValue[0].values[0].Latitude;
+                    var lon2 = locFilteredValue[0].values[0].Longitude;
+                    x = projection([lat2, lon2])[0];
+                    console.log(x);
+                    return x;
+                }
             }
         })
         .attr("cy", function(d) {
+            // console.log(d.values[0].Latitude);
+            // console.log(d.values[0].Longitude);
             if (d.values[0].Latitude.length != 0) {
-                return projection([d.values[0].Latitude,d.values[0].Longitude])[1]
+            	y = projection([d.values[0].Latitude,d.values[0].Longitude])[1];
+                return y;
+            } else {
+                var locName = d.values[0].Location.split(',');
+                // console.log(locName[0]);
+                // console.log(locNodes);
+                var locFilteredValue = locNodes.filter(function(d) {
+                    if (d.key.toLowerCase() == locName[0].toLowerCase()) {
+                        return d.key;
+                    };
+                });
+                // console.log(locFilteredValue);
+                if (typeof(locFilteredValue[0]) !== 'undefined') {
+                    // console.log(locFilteredValue[0].values[0].Latitude);
+                    var lat2 = locFilteredValue[0].values[0].Latitude;
+                    var lon2 = locFilteredValue[0].values[0].Longitude;
+                    y = projection([lat2, lon2])[1];
+                    console.log(y);
+                    return y;
+                }
             }
         })
         .attr("class","point")
@@ -117,29 +172,32 @@ function updateChart(year) {
     // TODO: replace text by hover triggered tooltip window
 
     var div;
-    geo_point.on("mouseover", function() {
-        div = d3.select(this).append("text")
-          .text(loc)
-          .attr("x", x + 5)
-          .attr("y", y + 5)
-          .attr("class","text");
-        div.style("visibility", "visible"); })
-      .on("mouseout", function() {
-        div.style("visibility", "hidden"); });
-
+    if (x != 0) {
+      console.log(x);
+	    geo_point.on("mouseover", function() {
+          console.log("something");
+	        div = d3.select(this).append("text")
+	          .text(loc)
+	          .attr("x", x + 5)
+	          .attr("y", y + 5)
+	          .attr("class","text");
+	        div.style("visibility", "visible"); })
+	      .on("mouseout", function() {
+	        div.style("visibility", "hidden"); });
+	  }
     geo_point.exit().remove()
 
 }
 
 function click() {
     var latlon = projection.invert(d3.mouse(this));
-    console.log(latlon);
+    // console.log(latlon);
 }
 
 function projectPoints(lat,lon,loc) {
 
     if (typeof(lat) !== 'undefined') {
-      console.log(lat)
+      // console.log(lat)
 
       var geo_point = g.append("g").attr("class", "geo_point");
       var x = projection([lat,lon])[0];
